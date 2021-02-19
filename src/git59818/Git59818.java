675679552c55af2729a9704d7f3254ac20faeb59
caroc <caroc@192.168.1.71>
2021-02-19 14:16:09 -0600

    Mi primer repositorio

---
+++ src/git59818/Git59818.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git59818;
+
+/**
+ *
+ * @author caroc
+ */
+public class Git59818 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "CAROLINA" ;
+        String matricula = "59818" ;
+        System.out.println("SOY " + nombre + " Y MI MATRICULA ES " + matricula);
+        
+        //MI PRIMER REPOSITORIO 
+    }
+    
+}
